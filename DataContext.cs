f97b51830568a0c53ed9e245330ba60a0a8ee440
alper <alper.usss@gmail.com>
2020-01-12 16:40:13 +0300

    interfaqce DataContext

---
+++ DataContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UnitOfWorkDeneme
+{
+    public class DataContext:DbContext
+    {
+       
+       public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Models.Category> Categories { get; set; }
+        public DbSet<Models.Customer> Customers { get; set; }
+        public DbSet<Models.CustomerCustomerDemo> CustomerCustomerDemos { get; set; }
+        public DbSet<Models.CustomerDemographic> CustomerDemographics { get; set; }
+        public DbSet<Models.Employee> Employees { get; set; }
+        public DbSet<Models.EmployeeTerritory> EmployeeTerritories { get; set; }
+        public DbSet<Models.Order> Orders { get; set; }
+        public DbSet<Models.OrderDetail> OrderDetails { get; set; }
+        public DbSet<Models.Product> Products { get; set; }
+        public DbSet<Models.Region> Regions { get; set; }
+        public DbSet<Models.Shipper> Shippers { get; set; }
+        public DbSet<Models.Supplier> Suppliers { get; set; }
+        public DbSet<Models.Territory> Territories { get; set; }
+    }
+}
---
+++ Interfaces/IUnitOfWork.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UnitOfWorkDeneme.Interfaces
+{
+    public interface IUnitOfWork:IDisposable
+    {
+        ICategoryRepository CategoryRepository{ get; set; }
+        ICustomerCustomerDemoRepository CustomerCustomerDemoRepository{ get; set; }
+        ICustomerDemographicRepository CustomerDemographic{ get; set; }
+        ICustomerRepository CustomerRepository{ get; set; }
+        IEmployeeRepository EmployeeRepository{ get; set; }
+        IEmployeeTerritoryRepository EmployeeTerritoryRepository{ get; set; }
+        IOrderDetailRepository OrderDetailRepository{ get; set; }
+        IOrderRepository OrderRepository{ get; set; }
+        IProductRepository ProductRepository{ get; set; }
+        IRegionRepository RegionRepository{ get; set; }
+        IShipperRepository ShipperRepository{ get; set; }
+        ISupplierRepository SupplierRepository{ get; set; }
+        ITerritoryRepository TerritoryRepository{ get; set; }
+        int Complete();
+
+    }
+}
